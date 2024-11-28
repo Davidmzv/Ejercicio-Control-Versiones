d66f124c22c47c13953868c97598aed26d9ee5fc
Davidmzv <davidm.mv54@gmail.com>
2024-11-28 20:06:35 +0100

    Dividir y multiplicar

--- Main.java
+++ Main.java
@@ -27,4 +27,15 @@ public class Main {
 		c = a - b;
 		return c;
 	}
+	
+	 public static int producto(int a, int b) {
+		 int c;
+		 c = a * b;
+		 return c;
+		 }
+	 public static int dividir(int a, int b) {
+		 int c;
+		 c = a / b;
+		 return c;
+		 }
 }
